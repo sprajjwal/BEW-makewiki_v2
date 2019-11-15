0139f541d77a529f03a2743730b902d3818ba804
sprajjwal <shashwatprajjwal@gmail.com>
2019-11-15 13:50:48 -0800

    added new page

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from wiki.views import PageListView, PageDetailView
+from wiki.views import PageListView, PageDetailView, PageNew
 
 
 urlpatterns = [
     path('', PageListView.as_view(), name='wiki-list-page'),
+    path('submit/', PageNew.as_view(), name="submit-wiki"),
     path('<str:slug>/', PageDetailView.as_view(), name='wiki-details-page'),
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView 
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+from wiki.forms import PageForm
 from wiki.models import Page
 
 
@@ -27,3 +31,25 @@ class PageDetailView(DetailView):
         return render(request, 'page.html', {
           'page': page
         })
+
+class PageNew(CreateView):
+    model = Page
+
+    def get(self, request):
+      """ Prints the form """
+      page_form = PageForm()
+      return render(request, 'add_page.html', {
+        'page_form': page_form
+      })
+
+    def post(self, request):
+      """processes the form and adds a wiki """
+      form = PageForm(request.POST)
+      form.instance.author = self.request.user
+      print(form)
+      if form.is_valid():
+        page = form.save()
+        return HttpResponseRedirect(reverse('wiki-details-page', args=[page.slug] ))
+      else:
+        print("CRI__________________________-")
+
