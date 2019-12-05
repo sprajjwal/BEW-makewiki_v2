cccaa9be5389507a42b25c9c25f9fc02ffd8def1
sprajjwal <shashwatprajjwal@gmail.com>
2019-12-05 14:08:28 -0800

    first push

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1yct-t!2bnkgc7j59z+9cdd2k)@y+ftqor$!aya()3if^cnlo-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','makeviki-shaash.herokuapp.com']
 
 
 # Application definition
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'wiki'
+    'wiki',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, "static"),
+]
 
 # wiki app settings
 WIKI_PAGE_TITLE_MAX_LENGTH = 600
--- makewiki/urls.py
+++ makewiki/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 """
 CHALLENGES:
@@ -34,4 +36,4 @@ urlpatterns = [
 
     # accounts app 
     path('accounts/', include('accounts.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
